79eec8cfb46864b716610961a7a03e75ab6a1006
Vitold S <vit1251@gmail.com>
2022-06-24 20:56:42 +0300

    Parsing command arguments

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # usm
+
 Universal Schema Migration is DB migration utility to provide way to JavaScript developer keep DB schema is updated
+
+## Usage workflow
+
+1. Create settings
+2. Create migration and commit it
+3. Exchange migraions with version control system
+4. Keep you database updated!
+
+## Settings options
+
+You may setup you `usm` migration project with additonal options by reading MySQL
+driver documentation at https://github.com/mysqljs/mysql#connection-options
+
+## Initialize migration project
+
+You may initialize `usm` project with using command line utility with interactive mode:
+
+    $ usm init
+
+## Migration routines
+
+You may migrate you database with next commands:
+
+    $ usm up 1
+
+or without steps count:
+
+    $ usm up
+
+This procdure update you MySQL instance up to latest migrations.
+
+## Contribution and contacts
+
+Please use GitHub contribution mechanism like disscussions or isses with you feedback at https://github.com/vit1251/usm.
+
+Thanks.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
+import { Command } from 'commander';
+import { createConnection } from 'mysql';
 
-const search = async () => {
+const program = new Command();
+
+program
+  .name('usm')
+  .description('CLI to universal schema migration utilities')
+  .version('1.0.0');
+
+program.command('init')
+  .description('Initialize USM project in place')
+  .action(async () => {
+    console.log(step);
+    console.log(options);
+  });
+
+program.command('up')
+  .description('Migration up')
+  .argument('<step>', 'how many steps migration should do')
+  .option('-v, --verbose', 'show additional debug messages and performance benchmarks')
+  .action(async (step, options) => {
+    console.log(step);
+    console.log(options);
+    
     const migration = './migration/20220624.js';
     const v = await import(migration);
     console.log(v);
@@ -8,6 +31,38 @@ const search = async () => {
     const {migrateUp, migrateDown} = m;
     console.log(migrateUp);
     await migrateUp('as');
-};
+    
+  });
 
-search();
+program.command('down')
+  .description('Migration down')
+  .action(async (step, options) => {
+    console.log(step);
+    console.log(options);
+  });
+
+program.command('check')
+  .description('Check MySQL connection')
+  .action(async (step, options) => {
+
+    const connection = createConnection({
+        host     : 'localhost',
+        user     : 'me',
+        password : 'secret',
+        database : 'my_db'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+    });
+
+    connection.end();
+
+
+  });
+
+
+program.parse();
